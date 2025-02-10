cb2f557ddcf83adee874b02c6f4e05867cb57cc6
iiEnchant <nawalmaeruf2@gmail.com>
2025-02-10 12:07:44 +0000

    Install and configure node-polyfill-webpack-plugin

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import nodePolyfills from "rollup-plugin-node-polyfills";
-import inject from "@rollup/plugin-inject";
+import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills(),
-    inject({
-      // Example: Provide global variables to modules
-      $: "jquery",
-      jQuery: "jquery",
-      _: "lodash",
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       crypto: "crypto-browserify",
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [new NodePolyfillPlugin()],
     },
   },
 });
