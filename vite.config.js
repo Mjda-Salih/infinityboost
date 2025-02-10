ab2fbc09406d592dc7f823f3ab16ab9c4542a03c
iiEnchant <nawalmaeruf2@gmail.com>
2025-02-10 12:02:51 +0000

    Install and configure rollup-plugin-polyfill-node

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-// Polyfill for the crypto module
 import nodePolyfills from "rollup-plugin-polyfill-node";
 
 // https://vitejs.dev/config/
