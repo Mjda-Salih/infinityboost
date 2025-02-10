de0eb5ff7d3c3a878ee5a7269d9728c8b842b5bf
iiEnchant <nawalmaeruf2@gmail.com>
2025-02-10 12:05:51 +0000

    Replace deprecated packages with recommended alternatives

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import nodePolyfills from "rollup-plugin-polyfill-node";
+import nodePolyfills from "rollup-plugin-node-polyfills";
+import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [
+    react(),
+    nodePolyfills(),
+    inject({
+      // Example: Provide global variables to modules
+      $: "jquery",
+      jQuery: "jquery",
+      _: "lodash",
+    }),
+  ],
   resolve: {
     alias: {
       crypto: "crypto-browserify",
